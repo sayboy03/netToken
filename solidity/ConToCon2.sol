315d745828de0a945d64becf9be82ad10da98201
sayboy03 <seongjin10@naver.com>
2018-04-25 16:25:46 +0900

    Update ConToCon2.sol

--- solidity/ConToCon2.sol
+++ solidity/ConToCon2.sol
@@ -3,33 +3,38 @@ pragma solidity ^0.4.16;
 
 contract Factory {
     address[] public newContracts;
+    //address public lastContract;
+    //uint256 public contractCount;
     
-    function createContract (uint256 tripId) {
-        address newContract = new Contract(tripId);
+    function createContract () {
+        address newContract = new Contract();
         newContracts.push(newContract);
-    } 
+    }
+    
+    function getAddr() public constant returns (address[]) {
+        return newContracts;
+    }
+    
+    function getLastAddr() public constant returns (uint256) {
+           uint256 contractCount = newContracts.length;
+        return contractCount;
+    }
+    
 }
 
 contract Contract {
-    uint256 public TripId;
     string public tempData;
-
-    function Contract (uint256 tripId) {
-        TripId = tripId;
+    
+    function Contract () {
+        
     }
 
-    function set(uint256 x, string y) public {
-        TripId = x;
+    function set(string y) public {
         tempData = y;
-    }
-
-    function get() public constant returns (uint256) {
-        return TripId;
     }
     
     function getData() public constant returns (string) {
         return tempData;
     }
-    
-}
 
+}
